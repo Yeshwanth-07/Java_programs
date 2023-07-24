2b94545c885dc9e3652cfbcd89342f81cb016e1a
Yeshwanth-07 <chikkalayeshwanth@gmail.com>
2023-07-24 12:47:37 +0530

    abcdefhello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,16 @@
-iorghifubkjfdnjbhjb mvmvnkj lvmkfdbv
+import java.util.*;
+public class Sum
+{
+ public static void main(String args[])
+ {
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter a number: ");
+  int n = sc.nextInt();
+  int sum=0;
+  for(int i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+  System.out.println("The sum upto n is: "+sum);
+ }
+}
